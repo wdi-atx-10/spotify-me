1901573eabc07765c05a433e805ae2240c03dea8
Remington Griffin <remington@utexas.edu>
2017-03-24 02:46:54 -0500

    everybody has choices...

--- js/spotify.js
+++ js/spotify.js
@@ -1,107 +1,48 @@
-// API Docs at:
-// https://developer.spotify.com/web-api/search-item/
-$(function(){
-  // console.log(searchByArtist('beyonce'));
-  // console.log(searchByTrack('always'));
-  $('#search-keyword').on('submit', function(event){
-     event.preventDefault();
-     console.log(search('beyonce'));
-  //    console.log('please');
-  //  if($('#search-type').val() == 'artist' ){
-  // //   $('#search-keyword').on('submit', function(event){
-  // //      event.preventDefault();
-  //       // console.log(searchByArtist());
-  // //     })
-  //     console.log('thanks');
-  //    }
-  //  else{$('#search-type').val('track')
-  // //   $('#search-keyword').on('submit', function(event){
-  // //      event.preventDefault();
-  //       console.log(searchByTrack());
-  //     })
-     }
 
-   )
- })
-//})
-//
-// function searchType(){
-//   // var artist = $('#search-type.artist');
-//   // var track = $('#search-type.track');
-//   // var search = $('#search-keyword').on('submit', function(event){
-//   //    event.preventDefault();
-//   //  })
-//   if($('#search-type').val('artist')){
-//     $('#search-keyword').on('submit', function(event){
-//        event.preventDefault();
-//        searchByArtist();
-//       })
-//     }
-//   else{$('#search-type').val('track')
-//     $('#search-keyword').on('submit', function(event){
-//        event.preventDefault();
-//        searchByTrack();
-//       })
-//     }
-// }
+var offset = 0;
+var type = $('#search-type').val();
+$(function(){
+  eventHandlers();
+})
 
 function search(){
   var url = 'https://api.spotify.com/v1/search';
   var data = {
     q: $('#search-keyword').val(),
-    type: $('#search-type').val()
+    type: $('#search-type').val(),
+    offset: offset ? offset : 0
   };
-  $.ajax({
+  a = $.ajax({
     url: url,
+    type: 'GET',
+    dataType: 'json',
     data: data
   })
   .done(function(response){
-    var result = response.value();
-    $('#results').append('<li>'+result+'</li>');
+    var list = $('#results');
+    $('#results').html('');
+    function artist(){ response.artists.items.forEach(function(a){
+      list.append('<li>' + a.name + '</li>');
+       })
+     }
+    function track(){ response.tracks.items.forEach(function(a){
+      list.append('<li>' + a.name + '</li>');
+       })
+     }
+
+    $('#search-type').val('artist')(artist());
+    $('#search-type').val('track')(track());
+
     console.log('response',response);
   })
+
   .fail(function(error){
     console.log('error',error);
   })
 }
-
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-  var data = {
-    q: $('#search-keyword').val()
-  };
-  $.ajax({
-    url: url,
-    data: data
-
-  })
-  .done(function(response){
-    var result = response.value;
-    $('#results').append('<li>'+result+'</li>');
-    console.log('response',response);
-  })
-  .fail(function(error){
-    console.log('error',error);
-  })
-}
-
-
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-  var data = {
-    q: $('#search-keyword').val()
-  };
-  $.ajax({
-    url: url,
-    data: data
-
-  })
-  .done(function(response){
-    var result = response.value();
-    $('#results').append('<li>'+result+'</li>');
-    console.log('response',response);
-  })
-  .fail(function(error){
-    console.log('error',error);
+function eventHandlers(){
+  $('#submitMe').on('click', function(e){
+    e.preventDefault();
+    search();
   })
 }
