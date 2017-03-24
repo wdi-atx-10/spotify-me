40f2eb3505599062e74059a96faaea7119b9562d
Robert Saunders <rtsaunders19@gmail.com>
2017-03-24 08:46:06 -0400

    try

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,47 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+(function(){
+  $('#search').on('submit', function(e){
+      e.preventDefault();
+      searchByArtist();
+    })
+
+
+
+})
 
 
 function searchByArtist(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+  var data = {};
+
+  var artistSearch = $('#search-keyword').val()
+
+$.ajax({
+    url: apiUrl,
+    data: data
+
+  })
+.done(function(response){
+
+
+
+  console.log(response);
+  })
+  .fail(function(error){
+  console.log('error', error)
+  })
+  .always(function(){
+  console.log("I always work!");
+  })
 }
+
+
+
 
 
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+}
+
 }
