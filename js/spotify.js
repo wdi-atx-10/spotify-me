6c3fbfba213be6dc7c1b15188c95b8f66409f763
Charles Orlando <cmo1991@gmail.com>
2017-03-23 21:49:19 -0500

    added next and back buttons

--- js/spotify.js
+++ js/spotify.js
@@ -1,34 +1,72 @@
-// API Docs at:
-// https://developer.spotify.com/web-api/search-item/
+var offset = 0;
 
-
-function search(keyword, type) {
+function search(offset) {
   var url = 'https://api.spotify.com/v1/search';
+  var keyword = $('#search-keyword').val();
+  var type = $('#search-type').val();
+
   $.ajax({
     url: url,
     data:{
       q: keyword,
-      type: type
+      type: type,
+      offset: offset
     }
   })
     .done(function(response){
       console.log(response);
-      $('#results').html('');
+      $('#results, p, a, .next, .previous').html('');
       if (type == 'artist'){
         var searchArray = response.artists.items;
         searchArray.forEach(function(artist){
           var li = $('<li />');
           li.text(artist.name);
           $('#results').append(li);
+          var showing = searchArray.length + offset;
+          $('p').text('Showing ' + offset + '-'+ showing + ' of ' + response.artists.total);
         });
+        if (response.artists.total > 20){
+          $('.next').show();
+          $('.next').text('next');
+          $('.next').click(function(){
+            offset= offset + 20;
+            search(offset);
+            });
+          if (offset > 0){
+            $('.previous').show();
+            $('.previous').text('previous');
+            $('.previous').click(function(){
+              offset= offset - 20;
+              search(offset);
+            });
+          };
+        };
       } else{
-        var searchArray = response.tracks.items;
-        searchArray.forEach(function(tracks){
-          var li = $('<li />');
-          li.text(tracks.name);
-          $('#results').append(li);
-        });
-      };
+          var searchArray = response.tracks.items;
+          searchArray.forEach(function(tracks){
+            var li = $('<li />');
+            li.text(tracks.name);
+            $('#results').append(li);
+            var showing = searchArray.length + offset;
+            $('p').text('Showing ' + offset + '-' + showing + ' of ' + response.artists.total);
+            if (response.tracks.total > 20){
+              $('.next').show();
+              $('.next').text('next');
+              $('.next').click(function(){
+                offset= offset + 20;
+                search(offset);
+                });
+              if (offset > 0){
+                $('.previous').show();
+                $('.previous').text('previous');
+                $('.previous').click(function(){
+                  offset= offset - 20;
+                  search(offset);
+                });
+              };
+            };
+          });
+        };
     })
     .fail(function(error){
       console.log(error);
@@ -36,20 +74,16 @@ function search(keyword, type) {
 
 }
 
-
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
-
 function main(){
   $('#search').on('submit',function(e){
+    $('.next, .previous').hide();
     e.preventDefault();
-    var keyword = $('#search-keyword').val();
-    var type = $('#search-type').val();
-    search(keyword, type);
+    offset = 0;
+    search(offset);
   });
 }
 
 $(function(){
+  $('.next, .previous').hide();
   main();
 });
