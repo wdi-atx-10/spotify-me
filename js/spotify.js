0e70a5a109fe1493b32bfb3c693d006228383464
Robert Saunders <rtsaunders19@gmail.com>
2017-06-22 12:35:48 -0500

    changes

--- js/spotify.js
+++ js/spotify.js
@@ -1,9 +1,12 @@
 // API Docs at:
 // https://developer.spotify.com/web-api/search-item/
-(function(){
+$(function(){
   $('#search').on('submit', function(e){
+      console.log('ok!');
       e.preventDefault();
-      searchByArtist();
+      $('ul').html('');
+      var artistSearch = $('#search-keyword').val();
+      searchByArtist(artistSearch);
     })
 
 
@@ -12,21 +15,29 @@
 
 
 function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-  var data = {};
+  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist,track';
 
-  var artistSearch = $('#search-keyword').val()
+
+
 
 $.ajax({
-    url: apiUrl,
-    data: data
-
+    url: url
   })
 .done(function(response){
-
-
-
   console.log(response);
+
+  for (i=0;i<5;i++) {
+    var trackArray = response.tracks.items[i];
+    var trackTitle = trackArray.name;
+    var trackArtist = trackArray.artists[0].name;
+    var trackAlbum = trackArray.album.name;
+    var artistArray = response.artists.items[i];
+    var artistName = artistArray.name
+    var artistUrl = artistArray.images[0].url
+  $('#song-results').append('<li>'+trackTitle+'<br>'+trackArtist+'<br>'+trackAlbum+'</li>');
+  $('#artist-results').append('<li><img src="'+artistUrl+'" height="100px" width="100px"/></li><br/>'+artistName)
+
+}
   })
   .fail(function(error){
   console.log('error', error)
@@ -40,8 +51,9 @@ $.ajax({
 
 
 
+/*
 function searchByTrack(keyword) {
   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
 }
 
-}
+}*/
